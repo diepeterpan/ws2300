7f1c7a9a6f7a6cecb2395ccfd6e2b4ee89ef2346
Die Peter Pan <diepeterpan@gmail.com>
2025-12-03 12:38:50 +0200

    Try and find error

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -12,15 +12,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let ws2300 = ws2300::Device::new(opt.device)?;
 
-    let data = match ws2300.read_all() {
-        Ok(data) => data,
-        Err(err) => panic!("Read error: {err}"),
-    };
+    let data = ws2300.read_all()?;
 
-    match serde_json::to_string(&data) {
-        Ok(json) => println!("{json}"),
-        Err(err) => panic!("JSON error: {err}"),
-    };
+    let json = serde_json::to_string(&data)?;
+    println!("{json}");
 
     Ok(())
 }
